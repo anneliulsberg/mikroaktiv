cf553459879d30262dd8942e492fc741b4d24bda
anneliulsberg <anneli@ulsberg.no>
2013-06-10 01:15:10 +0200

    Added content and styling to the content in index

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
 <?php get_header(); ?>
+<?php $categories = get_categories(array('orderby' => 'name', 'order' => 'ASC', 'child_of' => 0)); ?>
+<?php foreach ($categories as $category) : ?>
+
+	<div id="<?php echo $category->slug ?>">
+		<h2>Topp 5 <?php echo $category->name ?></h2>
+	
+		<ul>
+			<?php foreach (get_posts(array('numberposts' => 5, 'category' => $category->term_id)) as $post) : ?>
+			
+			<li><h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3></li>
+			
+			<?php endforeach; ?>
+		</ul>
+	
+	</div>
+
+<?php endforeach; ?>
 <?php get_footer(); ?>
