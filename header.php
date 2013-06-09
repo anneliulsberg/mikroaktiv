2b84f26d4b4b2790ae72f6bb1d6145858fa0ef77
anneliulsberg <anneli@ulsberg.no>
2013-06-09 14:43:56 +0200

    Added title

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 
 	<head>
 		<meta charset="UTF-8">
-		<title></title>
+		<title><?php wp_title('-', true, 'right'); ?> <?php bloginfo('name') ?></title>
 		<link rel="Shortcut Icon" type="image/ico" href="images/favicon.ico">
 		<link href='http://fonts.googleapis.com/css?family=Alef:400,700' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" href="css/index.css">
