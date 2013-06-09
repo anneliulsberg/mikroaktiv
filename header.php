e29fbb1effcef331e1d472380278b722f1b9901f
anneliulsberg <anneli@ulsberg.no>
2013-06-09 23:42:43 +0200

    Finished the slideshow
    Made a var for for code that repeated it self.
    Made changes in the html for slideshow.
    Added the cycle plugin for the slideshow in the html.
    Moved the decor to body to make it bigger and styled the slideshow.

--- header.php
+++ header.php
@@ -1,3 +1,4 @@
+<?php $t_uri = get_template_directory_uri(); ?>
 <!DOCTYPE HTML>
 <html lang="nb-no">
 
@@ -7,15 +8,21 @@
 		<link rel="Shortcut Icon" type="image/ico" href="images/favicon.ico">
 		<link href='http://fonts.googleapis.com/css?family=Alef:400,700' rel='stylesheet' type='text/css'>
 		<link href='http://fonts.googleapis.com/css?family=Finger+Paint' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/style.css">
+		<link rel="stylesheet" href="<?php echo $t_uri ?>/style.css">
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-		<script src="<?php echo get_template_directory_uri() ?>/js/main.js" type="text/javascript"></script>
+		<script src="<?php echo $t_uri ?>/js/jquery.cycle.js" type="text/javascript"></script>
+		<script src="<?php echo $t_uri ?>/js/main.js" type="text/javascript"></script>
 	</head>
 	
 	<body>
-		<header><a href="/"><img src="<?php echo get_template_directory_uri() ?>/images/menu-header-logo.png" alt="mikroaktiv" width="338" height="86"></a></header>
+		<header><a href="/"><img src="<?php echo $t_uri ?>/images/menu-header-logo.png" alt="mikroaktiv" width="338" height="86"></a></header>
 		
-		<div id="slideshow"></div>
+			<ul id="slideshow">
+				<li><a href="/aktiviteter"><img src="<?php echo $t_uri ?>/images/slideshow-blocks.jpg"></a></li>	
+				<li><a href="/aktiviteter"><img src="<?php echo $t_uri ?>/images/slideshow-chalk.jpg"></a></li>
+				<li><a href="/aktiviteter"><img src="<?php echo $t_uri ?>/images/slideshow-crayon.jpg"></a></li>
+				<li><a href="/aktiviteter"><img src="<?php echo $t_uri ?>/images/slideshow-fingerpaint.jpg"></a></li>	
+			</ul>
 		
 		<nav>
 			<ul>
