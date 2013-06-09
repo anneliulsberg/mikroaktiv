409625f12a9e9f20710940ff26554451afe0a710
anneliulsberg <anneli@ulsberg.no>
2013-06-09 14:16:52 +0200

    Initial commit
    The start of the mikroaktiv website

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="nb-no">
+
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+		<link rel="Shortcut Icon" type="image/ico" href="images/favicon.ico">
+		<link href='http://fonts.googleapis.com/css?family=Alef:400,700' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/index.css">
+	</head>
+	
+	<body>
