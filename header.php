f33c0af17876c3d77a8c1f2e156a76004ac7a658
anneliulsberg <anneli@ulsberg.no>
2013-06-09 17:51:53 +0200

    Added menu

--- header.php
+++ header.php
@@ -11,3 +11,12 @@
 	
 	<body>
 		<header><a href="/"><img src="<?php echo get_template_directory_uri() ?>/images/menu-header-logo.png" alt="mikroaktiv" width="338" height="86"></a></header>
+		
+		<nav>
+			<ul>
+				<li><a href="/aktiviteter">Aktiviteter</a></li>
+				<li><a href="/sanger">Sanger</a></li>
+				<li><a href="/rim-og-regler">Rim &amp; regler</a></li>
+				<li><a href="/eventyr">Eventyr</a></li>
+			</ul>
+		</nav>
