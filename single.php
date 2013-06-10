d3e5c5a89fa1558957f090690764d84db0aa2806
anneliulsberg <anneli@ulsberg.no>
2013-06-11 01:49:32 +0200

    Added div to style img in article

--- single.php
+++ single.php
@@ -3,9 +3,9 @@
 
 	<article>
 		<h1><?php the_title() ?></h1>
+		<div id="media"><?php echo get_the_post_thumbnail() ?></div>
 		<?php the_content() ?>	
 	</article>	
-
 
 <?php endwhile; ?>
 <?php get_footer(); ?>
