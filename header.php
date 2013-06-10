f66590b5ec0cae95556a39e3409e0f97ceee13e3
anneliulsberg <anneli@ulsberg.no>
2013-06-10 18:33:45 +0200

    Added twitter and facebook buttons

--- header.php
+++ header.php
@@ -16,6 +16,15 @@
 	</head>
 	
 	<body class="<?php echo is_home() ? 'home' : '' ?>">
+		<div id="fb-root"></div>
+		<script>(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = "//connect.facebook.net/nb_NO/all.js#xfbml=1";
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
+		
 		<header><a href="/">mikroaktiv</a></header>
 		
 		<?php if (is_home()) : ?>
