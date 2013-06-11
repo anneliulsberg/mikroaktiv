347260ab4d87d1306c8517573180492fbd4209f1
anneliulsberg <anneli@ulsberg.no>
2013-06-11 21:44:13 +0200

    moved the a element

--- content.php
+++ content.php
@@ -1,1 +1,5 @@
-<a href="<?php the_permalink(); ?>"><article><h3><?php the_title(); ?></h3></article></a>
+<article>
+	<h3><?php the_title(); ?></h3>
+
+	<a href="<?php the_permalink(); ?>"><?php echo get_the_post_thumbnail(); ?></a>
+</article>
