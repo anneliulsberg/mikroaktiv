16602e818966f48fe5f1f1eafc5b1bd80b6c09ac
anneliulsberg <anneli@ulsberg.no>
2013-06-13 01:29:22 +0200

    Added class for more specefic styling and styled it

--- content.php
+++ content.php
@@ -1,5 +1,5 @@
 <article style="background-image: url(<?php the_post_thumbnail_url(); ?>)">
-	<a href="<?php the_permalink(); ?>">
+	<a href="<?php the_permalink(); ?>" class="permalink">
 		<h3><?php the_title(); ?></h3>
 		<div class="post-ratings">
 			<?php echo the_ratings_results($post->ID); ?>
