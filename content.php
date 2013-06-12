94f6566f77f5aa24f4fdb234f0eb03ebf520006b
anneliulsberg <anneli@ulsberg.no>
2013-06-12 03:13:52 +0200

    Added and styled rating

--- content.php
+++ content.php
@@ -1,3 +1,8 @@
 <article style="background-image: url(<?php the_post_thumbnail_url(); ?>)">
-	<a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
+	<a href="<?php the_permalink(); ?>">
+		<h3><?php the_title(); ?></h3>
+		<div class="post-ratings">
+			<?php echo the_ratings_results($post->ID); ?>
+		</div>
+	</a>
 </article>
--- single.php
+++ single.php
@@ -2,7 +2,13 @@
 <?php while (have_posts()) : the_post(); ?>
 
 	<article>
-		<h1><?php the_title() ?></h1>
+		
+		<div id="header">
+			<h1><?php the_title() ?></h1>
+			<?php the_ratings() ?>
+
+		</div>
+				
 		<div id="media"><?php echo get_the_post_thumbnail() ?></div>
 		<div id="body"><?php the_content() ?></div>	
 	</article>	
