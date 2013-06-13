607517e2b5b68f55fe079012174feae6a02fa26e
anneliulsberg <anneli@ulsberg.no>
2013-06-13 15:20:45 +0200

    Added metadata

--- single.php
+++ single.php
@@ -6,7 +6,15 @@
 		<div id="header">
 			<h1><?php the_title() ?></h1>
 			<?php the_ratings() ?>
-
+			
+			<div id="metadata">
+				Publisert <?php the_date(); ?> i 
+				<?php
+					$category = get_the_category();
+					echo '<a href="' . get_category_link($category[0]->term_id) . '">' . $category[0]->name . '</a> ';
+				?>
+			</div>
+			
 		</div>
 				
 		<div id="media"><?php echo get_the_post_thumbnail() ?></div>
