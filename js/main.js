85cd5fdf9f41ad617e8794f5bfb73e071904e1bb
anneliulsberg <anneli@ulsberg.no>
2013-06-13 14:43:28 +0200

    Added pager to cycle javascript

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-jQuery(function($) {	
-	"use strict";
-	
-	if (document.documentElement.clientWidth > 615) {
-		$('#slideshow').cycle();
-	}	
+jQuery(function($) {    
+    "use strict";
+    
+    if (document.documentElement.clientWidth > 615) {
+        $('#pager').append('<ol>');
+
+        $('#slideshow').cycle({
+            pager : '#pager ol',
+            activePagerClass : 'current',
+            pagerAnchorBuilder: function(idx, slide) { 
+                return '<li><a href="#"></a></li>'; 
+            } 
+        });
+    }    
 });
